b4ae301035e584bcf7e36ef4764d056cda876e8c
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2022-05-16 07:44:15 -1000

    Update index.js

--- versions/njs/src/index.js
+++ versions/njs/src/index.js
@@ -1,1 +1,10 @@
-// Leave empty, i can't code rn
+console.log("maCOS Builder")
+console.log("node.js version")
+const args = process.argv.slice(2)
+if (args[0] == "--javascript" || args[0] == "-js") {
+  if (args[1].endsWith(".js")) {
+    
+  } else {
+   console.log("Wrong format!") 
+  }
+}
